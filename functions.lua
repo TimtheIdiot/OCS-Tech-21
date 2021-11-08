40dc0a49ee07313cf78537cda461c83ad301ec2a
TimtheIdiot <90470015+TimtheIdiot@users.noreply.github.com>
2021-11-08 14:18:30 -0600

    updated functions.lua

--- functions.lua
+++ functions.lua
@@ -18,7 +18,7 @@ function aoe.placePad(x,z,pos)
         for j = 1, z, 1 do
             local node = minetest.get_node({x=pos.x+i,y=depth,z=pos.z+j})
             minetest.chat_send_all(node.name)
-            if node.name == "air" then
+            if node.name == "air" and node.name == "default:water_source" then
                minetest.chat_send_all("Error1")
                flag1 = false
             end
@@ -27,16 +27,42 @@ function aoe.placePad(x,z,pos)
                 -- place the pad
                 for i = 1, x, 1 do 
                     for j = 1, z, 1 do
-                        minetest.set_node({x=pos.x+i,y=pos.y,z=pos.z+j}, {name="default:dirt"})
+                        minetest.set_node({x=pos.x+i,y=pos.y,z=pos.z+j}, {name=node.name})
                     end
                 end
             end
-        end 
-    end
-
+        end
+    end 
 end
 
 
+
 function aoe.areaClear(x,z,y,pos)
+    minetest.chat_send_all("code running confirmed")
+    local depth = pos.y - math.floor(x*(.8))
+    for i = 1, x, 1 do 
+        for j = 1, 1, 1 do
+            local node = minetest.get_node({x=pos.x+i,y=depth,z=pos.z+j})
+            minetest.chat_send_all(node.name)
+            for i = 1, x, 1 do 
+                for j = 1, z, 1 do
+                    minetest.set_node({x=pos.x+i,y=pos.y,z=pos.z+j}, {name="air"})
+                end
+            end
+        end
+    end
+end
+
+function aoe.placeNode(pos)
+    
+        if minetest.get_node( pos ).name == "air" then
+        minetest.set_fluid(player:get_pos(), {name="aoe:poopy"})
+    end
+end
+
+function aoe.cokeBuff()
+    player:set_physics_override({speed = 5, sneak = false, jump = 5})--this grants the cocaine buff
+            timer:start(10) -- in seconds
+        on_timer = player:set_physics_override({speed = 1, sneak = true, jump = 1})
 
 end
